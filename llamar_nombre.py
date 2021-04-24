ebd50ea24cba7c9e0786c9a34ac58858dbbe82cf
Fiorella14 <fiorellagonzalesc@hotmail.com>
2021-04-24 09:33:25 -0500

    almacena nombre

---
+++ llamar_nombre.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 10 10:47:23 2021
+
+@author: fiore
+"""
+
+fnam = input("May I have your first name, please? ")
+lnam = input("May I have your last name, please? ")
+print("Thank you.")
+print("\nYour name is " + fnam + " " + lnam + ".")
