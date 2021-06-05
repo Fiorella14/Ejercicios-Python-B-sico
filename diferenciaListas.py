05d4c0418ce40d2a6fda312eca4218f6ef051328
Fiorella14 <fiorellagonzalesc@hotmail.com>
2021-06-04 23:50:32 -0500

    ejercicios listas

---
+++ diferenciaListas.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  4 22:51:33 2021
+
+@author: fiore
+"""
+
+Color1 = ["red", "orange", "green", "blue", "white"]
+Color2 = ["black", "yelow", "green", "blue"]
+
+diferencia = set(Color1)- set(Color2)
+print(diferencia)
